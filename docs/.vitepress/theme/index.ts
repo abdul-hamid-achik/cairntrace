2dceda48a0fc8d079d4b4d8908a9b026aa689d96
Abdul Hamid <abdulachik@icloud.com>
2026-07-14 09:40:22 -0600

    feat(docs): add Vercel Web Analytics

--- docs/.vitepress/theme/index.ts
+++ docs/.vitepress/theme/index.ts
@@ -1,4 +1,12 @@
+import { inject } from '@vercel/analytics'
 import DefaultTheme from 'vitepress/theme-without-fonts'
 import './custom.css'
 
-export default DefaultTheme
+export default {
+  extends: DefaultTheme,
+  enhanceApp() {
+    if (!import.meta.env.SSR) {
+      inject({ framework: 'vitepress' })
+    }
+  },
+}
